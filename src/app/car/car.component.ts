6342b844a68f06d4d4f3d9c538ed9d196f5ee773
Бодька <bbigun2000@gmail.com>
2021-01-21 16:25:41 +0200

    car

--- src/app/car/car.component.ts
+++ src/app/car/car.component.ts
@@ -1,4 +1,5 @@
 import {Component, Input, OnInit} from '@angular/core';
+import {ICars} from '../../interfaces/carsInterface/ICars';
 
 @Component({
   selector: 'app-car',
@@ -7,7 +8,7 @@ import {Component, Input, OnInit} from '@angular/core';
 })
 export class CarComponent implements OnInit {
   @Input()
-  car: any;
+  car: ICars;
   constructor() { }
 
   ngOnInit(): void {
