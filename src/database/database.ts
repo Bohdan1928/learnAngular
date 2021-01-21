6e8e6a40324bfc1e35e857217c0e4d459e93950a
Бодька <bbigun2000@gmail.com>
2021-01-21 12:32:01 +0200

    car

---
+++ src/database/database.ts
@@ -0,0 +1,9 @@
+export let cars = [
+  {producer: 'VW', year: 2009, price: 7000, power: 150},
+  {producer: 'BMW', year: 2008, price: 5000, power: 250},
+  {producer: 'VOLVO', year: 2010, price: 6000, power: 150},
+  {producer: 'VOLGA', year: 1992, price: 4000, power: 85},
+  {producer: 'VAZ', year: 1989, price: 1500, power: 70},
+  {producer: 'AUDI', year: 2012, price: 11000, power: 200},
+  {producer: 'FIAT', year: 2007, price: 8500, power: 100}
+];
